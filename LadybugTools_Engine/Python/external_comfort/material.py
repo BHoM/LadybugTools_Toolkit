4ee39f75fb86c7f4f19d5abec7d1b15f2bac398c
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-02-11 17:31:57 +0000

    Slowly adding methdos

---
+++ LadybugTools_Engine/Python/external_comfort/material.py
@@ -0,0 +1,7 @@
+from honeybee_energy.material.opaque import EnergyMaterial, EnergyMaterialVegetation
+from ladybug.epw import EPW
+from typing import List, Union
+
+
+if __name__ == "__main__":
+    pass
