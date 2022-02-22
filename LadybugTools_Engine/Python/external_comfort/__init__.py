7195981fd8dec74e75af29caacc1a55e0fa39235
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-02-22 19:57:14 +0000

    still not working, but at least it rebased

--- LadybugTools_Engine/Python/external_comfort/__init__.py
+++ LadybugTools_Engine/Python/external_comfort/__init__.py
@@ -12,11 +12,14 @@ TOOLKIT_ENVIRONMENT = Path(
 
 ladybug_tools_folder = Path(f"C:/Users/{USERNAME}/ladybug_tools")
 
+# TODO - Check that LBTools_Toolkit HB version matches that of the LB version
+
+
 hb_folders.default_simulation_folder = f"C:/Users/{USERNAME}/simulation"
-hb_folders._python_exe_path = (TOOLKIT_ENVIRONMENT / "python.exe").as_posix()
-hb_folders._python_package_path = (TOOLKIT_ENVIRONMENT / "Lib/site-packages").as_posix()
-hb_folders._python_scripts_path = (TOOLKIT_ENVIRONMENT / "Scripts").as_posix()
-QUEENBEE_EXE = (TOOLKIT_ENVIRONMENT / "Scripts/queenbee.exe").as_posix()
+hb_folders._python_exe_path = (ladybug_tools_folder / "python/python.exe").as_posix()
+hb_folders._python_package_path = (ladybug_tools_folder / "python/Lib/site-packages").as_posix()
+hb_folders._python_scripts_path = (ladybug_tools_folder / "python/Scripts").as_posix()
+QUEENBEE_EXE = (ladybug_tools_folder / "python/Scripts/queenbee.exe").as_posix()
 
 hbe_folders.openstudio_path = (ladybug_tools_folder / "openstudio/bin").as_posix()
 hbe_folders.energyplus_path = (
