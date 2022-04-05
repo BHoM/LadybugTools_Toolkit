e2bf5e7a6ec98b1608a238afeec2df36f11a6b07
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-04-05 13:23:50 +0100

    working prototype typology simulation

--- LadybugTools_Engine/Compute/ExternalComfort.cs
+++ LadybugTools_Engine/Compute/ExternalComfort.cs
@@ -29,17 +29,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using BH.oM.Base;
+using System.IO;
 
 namespace BH.Engine.LadybugTools
 {
     public static partial class Compute
     {
-        [Description("Run an Openfield simulation and return results.")]
+        [Description("Run an External Comfort simulation and return results.")]
         [Input("epw", "An EPW file.")]
         [Input("groundMaterial", "A pre-defined ground material.")]
         [Input("shadeMaterial", "A pre-defined shade material.")]
-        [Output("openfield", "An openfield object containing simulation results.")]
-        public static CustomObject Openfield(string epw, string groundMaterial = "ASPHALT", string shadeMaterial = "FABRIC")
+        [Output("externalComfortResult", "An external comfort result object containing simulation results.")]
+        public static CustomObject ExternalComfort(string epw, ExternalComfortMaterial groundMaterial, ExternalComfortMaterial shadeMaterial)
         {
             PythonEnvironment pythonEnvironment = Python.Query.LoadPythonEnvironment(Query.ToolkitName());
             if (!pythonEnvironment.IsInstalled())
@@ -48,57 +49,49 @@ namespace BH.Engine.LadybugTools
                 return null;
             }
 
+            if (!Query.ExternalComfortPossible())
+                return null;
+
+            if (groundMaterial == ExternalComfortMaterial.Undefined || shadeMaterial == ExternalComfortMaterial.Undefined)
+            {
+                BH.Engine.Base.Compute.RecordError($"Please input a valid ExternalComfortMaterial.");
+                return null;
+            }
+
+            if (!File.Exists(epw))
+            {
+                BH.Engine.Base.Compute.RecordError($"The EPW file given cannot be found.");
+                return null;
+            }
+
+            string outputPath = Path.Combine(Path.GetTempPath(), "ecr.json");
+
+            string epwPath = Path.GetFullPath(epw);
+
             string pythonScript = String.Join("\n", new List<string>() 
             {
                 "import sys",
                 $"sys.path.insert(0, '{pythonEnvironment.CodeDirectory()}')",
                 "",
-                "import json",
-                "from external_comfort.openfield import EPW, Openfield",
-                "import numpy as np",
+                "from ladybug.epw import EPW",
+                "from external_comfort.external_comfort import ExternalComfort, ExternalComfortResult",
+                "from external_comfort.material import MATERIALS",
                 "",
-                "class NpEncoder(json.JSONEncoder):",
-                "    def default(self, obj):",
-                "        if isinstance(obj, np.integer) :",
-                "            return int (obj)",
-                "        if isinstance(obj, np.floating) :",
-                "            return float (obj)",
-                "        if isinstance(obj, np.ndarray) :",
-                "            return obj.tolist()",
-                "        return super(NpEncoder, self).default(obj)",
-                "",
-                $"epw = EPW(r'{epw}')",
-                $"of = Openfield(epw, '{groundMaterial}', '{shadeMaterial}', run=True)",
-                "",
-                "d = {}",
-                $"d['EPW'] = r'{epw}'",
-                $"d['GroundMaterial'] = '{groundMaterial}'",
-                $"d['ShadeMaterial'] = '{shadeMaterial}'",
-
-                "d['ShadedGroundSurfaceTemperature'] = of.shaded_below_temperature.values",
-                "d['ShadeSurfaceTemperature'] = of.shaded_above_temperature.values",
-                "d['ShadedDirectRadiation'] = of.shaded_direct_radiation.values",
-                "d['ShadedDiffuseRadiation'] = of.shaded_diffuse_radiation.values",
-                "d['ShadedLongwaveRadiantTemperature'] = of.shaded_longwave_mean_radiant_temperature.values",
-                "d['ShadedMeanRadiantTemperature'] = of.shaded_mean_radiant_temperature.values",
-                "d['ShadedUniversalThermalClimateIndex'] = None",
-
-                "d['UnshadedGroundSurfaceTemperature'] = of.unshaded_below_temperature.values",
-                "d['SkyTemperature'] = of.unshaded_above_temperature.values",
-                "d['UnshadedDirectRadiation'] = of.unshaded_direct_radiation.values",
-                "d['UnshadedDiffuseRadiation'] = of.unshaded_diffuse_radiation.values",
-                "d['UnshadedLongwaveRadiantTemperature'] = of.unshaded_longwave_mean_radiant_temperature.values",
-                "d['UnshadedMeanRadiantTemperature'] = of.unshaded_mean_radiant_temperature.values",
-                "d['UnshadedUniversalThermalClimateIndex'] = None",
-
-                "print(json.dumps(d, cls=NpEncoder))",
+                $"epw = EPW(r'{epwPath}')",
+                $"ec = ExternalComfort(epw, ground_material=MATERIALS['{groundMaterial}'], shade_material=MATERIALS['{shadeMaterial}'])",
+                "ecr = ExternalComfortResult(ec)",
+                $"ecr.to_json(r'{outputPath}')",
+                "print('Nothing to see here!')",
             });
 
             string output = Python.Compute.RunPythonString(pythonEnvironment, pythonScript).Trim();
-            string jsonString = output.Split(
-                new string[] { System.Environment.NewLine },
-                StringSplitOptions.None
-            ).Last();
+
+            string jsonString = "";
+            using (StreamReader r = new StreamReader(outputPath))
+            {
+                jsonString = r.ReadToEnd();
+            }
+
 
             return Serialiser.Convert.FromJson(jsonString) as CustomObject;
         }
