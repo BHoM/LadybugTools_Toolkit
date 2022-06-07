95fb0b2acca99e85957de5fe6b4ee8c487887fa4
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-06-07 16:46:18 +0100

    Added PV TOF plot

--- LadybugTools_Engine/Python/ladybug_extension/epw.py
+++ LadybugTools_Engine/Python/ladybug_extension/epw.py
@@ -1,4 +1,7 @@
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import datetime
+import itertools
+import multiprocessing
 from typing import List
 
 import numpy as np
@@ -833,3 +836,34 @@ def _epw_equality(epw0: EPW, epw1: EPW, include_header: bool = False) -> bool:
             atm_match,
         ]
     )
+
+
+def radiation_tilt_orientation_matrix(epw: EPW) -> pd.DataFrame:
+    """Compute the annual cumulative radiation matrix per surface tilt and orientation, for a given EPW object.
+
+    Args:
+        epw (EPW): The EPW object for which this calculation is made.
+
+    Returns:
+        pd.DataFrame: _description_
+    """    
+    
+    wea = Wea.from_annual_values(epw.location, epw.direct_normal_radiation.values, epw.diffuse_horizontal_radiation.values, is_leap_year=epw.is_leap_year)
+    
+    # I implement a bit of a hack here, to calculate only the Eastern insolation - then mirror it about the North-South axis to get the whole matrix
+    altitudes = np.linspace(0, 90, 10)
+    azimuths = np.linspace(0, 180, 19)
+    combinations = np.array(list(itertools.product(altitudes, azimuths)))
+
+    def f(alt_az):
+        return wea.__copy__().directional_irradiance(alt_az[0], alt_az[1])[0].total
+    
+    with ThreadPoolExecutor() as executor:
+        results = np.array([i for i in executor.map(f, combinations[0:])]).reshape(len(altitudes), len(azimuths))
+
+    temp = pd.DataFrame(results, index=altitudes, columns=azimuths)
+
+    new_cols = (360 - temp.columns)[::-1][1:]
+    new_vals = temp.values[::-1, ::-1][::-1, 1:]  # some weird array transformation stuff here
+    mirrored = pd.DataFrame(new_vals, columns=new_cols, index=temp.index)
+    return pd.concat([temp, mirrored], axis=1)
--- LadybugTools_Engine/Python/ladybug_extension/plot.py
+++ LadybugTools_Engine/Python/ladybug_extension/plot.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import List, Union
 from datetime import datetime
 from pathlib import Path
+import pandas as pd
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 from ladybug.datacollection import HourlyContinuousCollection
@@ -506,6 +507,74 @@ def diurnal(
         )
 
     # Tidy plot
+    plt.tight_layout()
+
+    return fig
+
+
+def radiation_tilt_orientation(radiation_matrix: pd.DataFrame, title: str = None,) -> Figure:
+    """Convert a radiation matrix to a figure showing the radiation tilt and orientation.
+
+    Args:
+        radiation_matrix (pd.DataFrame): A matrix with altitude index, azimuth columns, and radiation values in Wh/m2.
+        title (str, optional): A title for the figure. Defaults to None.
+
+    Returns:
+        Figure: A figure.
+    """
+
+    # Construct input values
+    x = np.tile(radiation_matrix.index, [len(radiation_matrix.columns), 1]).T
+    y = np.tile(radiation_matrix.columns, [len(radiation_matrix.index), 1])
+    z = radiation_matrix.values
+
+    z_min = radiation_matrix.min().min()
+    z_max = radiation_matrix.max().max()
+    z_percent = z / z_max * 100
+
+    # Find location of max value
+    ind = np.unravel_index(np.argmax(z, axis=None), z.shape)
+
+    # Create figure
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+    cf = ax.contourf(y, x, z / 1000, cmap="YlOrRd", levels=51)
+    cl = ax.contour(y, x, z_percent, levels=[50, 60, 70, 80, 90, 95, 99], colors=["w"], linewidths=[1], alpha=0.75, linestyles=[":"])
+    ax.clabel(cl, fmt='%r %%')
+    
+    ax.scatter(y[ind], x[ind], c="k")
+    ax.text(y[ind]+2, x[ind]-2, f"{z_max / 1000:0.0f}kWh/m${{^2}}$/year", ha="left", va="top", c="w")
+
+    [
+        ax.spines[spine].set_visible(False)
+        for spine in ["top", "right"]
+    ]
+
+    ax.xaxis.set_major_locator(mtick.MultipleLocator(base=30))
+
+    ax.grid(b=True, which="major", color="white", linestyle=":", alpha=0.25)
+
+    cb = fig.colorbar(
+        cf,
+        orientation="vertical",
+        drawedges=False,
+        fraction=0.05,
+        aspect=25,
+        pad=0.02,
+        label="kWh/m${^2}$/year",
+    )
+    cb.outline.set_visible(False)
+    cb.add_lines(cl)
+    cb.locator = mtick.MaxNLocator(nbins=10, prune=None)
+
+    ax.set_xlabel("Panel orientation (clock-wise from North at 0°)")
+    ax.set_ylabel("Panel tilt (0° facing the horizon, 90° facing the sky)")
+
+    # Title
+    if title is None:
+        ax.set_title(f"Annual cumulative radiation", x=0, ha="left", va="bottom")
+    else:
+        ax.set_title(f"{title}\nAnnual cumulative radiation", x=0, ha="left", va="bottom")
+
     plt.tight_layout()
 
     return fig
