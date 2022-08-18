9a14d753e6ed447a062d7eaa1e26cd48ef79745f
Tristan Gerrish <tristan.gerrish@burohappold.com>
2022-08-18 20:50:11 +0100

    ...

--- LadybugTools_Engine/Python/src/ladybugtools_toolkit/external_comfort/cityscale/__init__.py
+++ LadybugTools_Engine/Python/src/ladybugtools_toolkit/external_comfort/cityscale/__init__.py
@@ -1,10 +1,5 @@
 import warnings
 
 warnings.warn(
-    f"Methods in {__file__} rely on external tools - namely OSGeo4W. This must be installed for this method to work!"
-)
-import warnings
-
-warnings.warn(
     f"The code in this module relies on QGIS in in order to process geotiff rasters. For this to work, you must have QGIS 3.26.1 installed."
 )
--- LadybugTools_Engine/Python/src/ladybugtools_toolkit/external_comfort/wind/wind_speed_at_height.py
+++ LadybugTools_Engine/Python/src/ladybugtools_toolkit/external_comfort/wind/wind_speed_at_height.py
@@ -11,14 +11,20 @@ def wind_speed_at_height(
     """Calculate the wind speed at a given height from the 10m default height as stated in an EPW file.
 
     Args:
-        reference_wind_speed (float): The speed to be translated.
-        reference_height (float): The original height of the wind speed being translated.
-        target_height (float): The target height of the wind speed being translated.
-        terrain_roughness_length (float): A value describing how rough the ground is.
-        log_function (bool, optional): Set to True to used the log transformation method, or False for the exponent method. Defaults to True.
+        reference_wind_speed (float):
+            The speed to be translated.
+        reference_height (float):
+            The original height of the wind speed being translated.
+        target_height (float):
+            The target height of the wind speed being translated.
+        terrain_roughness_length (float):
+            A value describing how rough the ground is.
+        log_function (bool, optional):
+            Set to True to used the log transformation method, or False for the exponent method. Defaults to True.
 
     Returns:
-        float: The translated wind speed at the target height.
+        float:
+            The translated wind speed at the target height.
     """
     if log_function:
         return reference_wind_speed * (
