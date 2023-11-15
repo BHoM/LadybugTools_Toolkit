52d0a84442e37f409e3473c365ba92465187a392
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-15 09:38:14 +0000

    Make paths Path objects instead of strings
    Co-authored-by: Tristan Gerrish <10939984+tg359@users.noreply.github.com>

--- LadybugTools_Engine/Python/src/ladybugtools_toolkit/bhom/__init__.py
+++ LadybugTools_Engine/Python/src/ladybugtools_toolkit/bhom/__init__.py
@@ -1,16 +1,16 @@
 """Root for the bhom subpackage."""
 
-#from pathlib import Path  # pylint: disable=E0401
+from pathlib import Path  # pylint: disable=E0401
 from os import path
 
 from win32api import HIWORD, LOWORD, GetFileVersionInfo
 
-BHOM_ASSEMBLIES_DIRECTORY = path.expandvars("%PROGRAMDATA%/BHoM/Assemblies")
-BHOM_DIRECTORY = path.expandvars("%PROGRAMDATA%/BHoM")
-BHOM_LOG_FOLDER = path.expandvars("%PROGRAMDATA%/BHoM/Logs")
+BHOM_ASSEMBLIES_DIRECTORY = Path(path.expandvars("%PROGRAMDATA%/BHoM/Assemblies"))
+BHOM_DIRECTORY = Path(path.expandvars("%PROGRAMDATA%/BHoM"))
+BHOM_LOG_FOLDER = Path(path.expandvars("%PROGRAMDATA%/BHoM/Logs"))
 
-PYTHON_CODE_DIRECTORY = path.expandvars("%PROGRAMDATA%/BHoM/Extensions/PythonCode")
-PYTHON_ENVIRONMENTS_DIRECTORY = path.expandvars("%PROGRAMDATA%/BHoM/Extensions/PythonEnvironments")
+PYTHON_CODE_DIRECTORY = Path(path.expandvars("%PROGRAMDATA%/BHoM/Extensions/PythonCode"))
+PYTHON_ENVIRONMENTS_DIRECTORY = Path(path.expandvars("%PROGRAMDATA%/BHoM/Extensions/PythonEnvironments"))
 
 TOOLKIT_NAME = "LadybugTools_Toolkit"
 
