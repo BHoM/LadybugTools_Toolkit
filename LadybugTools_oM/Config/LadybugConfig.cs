8bb08b95af43193d54a78c116fa087f3486f5b4a
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-22 12:23:03 +0000

    removed reference to deleted interface

--- LadybugTools_oM/Config/LadybugConfig.cs
+++ LadybugTools_oM/Config/LadybugConfig.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace BH.oM.LadybugTools
 {
-    public class LadybugConfig : ActionConfig, ILadybugConfig
+    public class LadybugConfig : ActionConfig
     {
         public virtual FileSettings JsonFile { get; set; } = null;
     }
