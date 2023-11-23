6bcf936bd12f719c1372bd2cb665d101e3ca2ec4
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-23 09:14:38 +0000

    I caught the ball from AnalysisPeriod

--- LadybugTools_Adapter/Convert/MetaData/AnalysisPeriod.cs
+++ LadybugTools_Adapter/Convert/MetaData/AnalysisPeriod.cs
@@ -32,25 +32,98 @@ namespace BH.Adapter.LadybugTools
     {
         public static BH.oM.LadybugTools.AnalysisPeriod ToAnalysisPeriod(Dictionary<string, object> oldObject)
         {
+            int startMonth = 1;
+            int startDay = 1;
+            int startHour = 0;
+            int endMonth = 12;
+            int endDay = 31;
+            int endHour = 23;
+            int timeStep = 1;
+            bool isLeapYear = false;
+
             try
             {
-                return new oM.LadybugTools.AnalysisPeriod()
-                {
-                    StartMonth = (int)oldObject["st_month"],
-                    StartDay = (int)oldObject["st_day"],
-                    StartHour = (int)oldObject["st_hour"],
-                    EndMonth = (int)oldObject["end_month"],
-                    EndDay = (int)oldObject["end_day"],
-                    EndHour = (int)oldObject["end_hour"],
-                    IsLeapYear = (bool)oldObject["is_leap_year"],
-                    TimeStep = (int)oldObject["timestep"]
-                };
+                startMonth = (int)oldObject["st_month"];
             }
             catch (Exception ex)
             {
-                BH.Engine.Base.Compute.RecordError($"An error occurred during conversion of a {typeof(AnalysisPeriod).FullName}. Returning a default {typeof(AnalysisPeriod).FullName}:\n The error: {ex}");
-                return new AnalysisPeriod();
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the start month of the AnalysisPeriod. returning start month as default ({startMonth}).\n The error: {ex}");
             }
+
+            try
+            {
+                startDay = (int)oldObject["st_day"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the start day of the AnalysisPeriod. returning start day as default ({startDay}).\n The error: {ex}");
+            }
+
+            try
+            {
+                startHour = (int)oldObject["st_hour"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the start hour of the AnalysisPeriod. returning start hour as default ({startHour}).\n The error: {ex}");
+            }
+
+            try
+            {
+                endMonth = (int)oldObject["end_month"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the end month of the AnalysisPeriod. returning end month as default ({endMonth}).\n The error: {ex}");
+            }
+
+            try
+            {
+                endDay = (int)oldObject["end_day"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the end day of the AnalysisPeriod. returning end day as default ({endDay}).\n The error: {ex}");
+            }
+
+            try
+            {
+                endHour = (int)oldObject["end_hour"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the end hour of the AnalysisPeriod. returning end hour as default ({endHour}).\n The error: {ex}");
+            }
+
+            try
+            {
+                isLeapYear = (bool)oldObject["is_leap_year"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when trying to determine whether the AnalysisPeriod is a leap year. returning IsLeapYear as default (false).\n The error: {ex}");
+            }
+
+            try
+            {
+                timeStep = (int)oldObject["timestep"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the time step of the AnalysisPeriod. returning time step as default ({timeStep}).\n The error: {ex}");
+            }
+
+            return new oM.LadybugTools.AnalysisPeriod()
+            {
+                StartMonth = startMonth,
+                StartDay = startDay,
+                StartHour = startHour,
+                EndMonth = endMonth,
+                EndDay = endDay,
+                EndHour = endHour,
+                IsLeapYear = isLeapYear,
+                TimeStep = timeStep
+            };
         }
 
         public static Dictionary<string, object> FromAnalysisPeriod(BH.oM.LadybugTools.AnalysisPeriod analysisPeriod)
