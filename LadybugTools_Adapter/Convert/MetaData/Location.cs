445f5bf89769d272655ee45c9fd6842e2ecd6201
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-23 09:58:25 +0000

    we found the Location that we're playing catch

--- LadybugTools_Adapter/Convert/MetaData/Location.cs
+++ LadybugTools_Adapter/Convert/MetaData/Location.cs
@@ -31,26 +31,109 @@ namespace BH.Adapter.LadybugTools
     {
         public static BH.oM.LadybugTools.Location ToLocation(Dictionary<string, object> oldObject)
         {
+            string city = "";
+            string state = "";
+            string country = "";
+            double latitude = 0.0;
+            double longitude = 0.0;
+            double timeZone = 0.0;
+            double elevation = 0.0;
+            string stationID = "";
+            string source = "BHoM LadybugTools_Toolkit default";
+
             try
             {
-                return new oM.LadybugTools.Location()
-                {
-                    City = (string)oldObject["city"],
-                    State = (string)oldObject["state"],
-                    Country = (string)oldObject["country"],
-                    Latitude = (double)oldObject["latitude"],
-                    Longitude = (double)oldObject["longitude"],
-                    TimeZone = (double)oldObject["time_zone"],
-                    Elevation = (double)oldObject["elevation"],
-                    StationId = (string)oldObject["station_id"],
-                    Source = (string)oldObject["source"]
-                };
+                city = (string)oldObject["city"];
             }
             catch (Exception ex)
             {
-                BH.Engine.Base.Compute.RecordError($"An error occurred during conversion of a {typeof(Location).FullName}. Returning a default {typeof(Location).FullName}:\n The error: {ex}");
-                return new Location();
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the city name of the Location. returning city as default (\"\").\n The error: {ex}");
             }
+
+            try
+            {
+                state = (string)oldObject["state"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the state name of the Location. returning state as default (\"\").\n The error: {ex}");
+            }
+
+            try
+            {
+                country = (string)oldObject["country"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the country name of the Location. returning country as default (\"\").\n The error: {ex}");
+            }
+
+            try
+            {
+                latitude = (double)oldObject["latitude"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the latitude of the Location. returning latitude as default ({latitude}).\n The error: {ex}");
+            }
+
+            try
+            {
+                longitude = (double)oldObject["longitude"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the longitude of the Location. returning longitude as default ({longitude}).\n The error: {ex}");
+            }
+
+            try
+            {
+                timeZone = (double)oldObject["time_zone"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the time zone of the Location. returning time zone as default ({timeZone}).\n The error: {ex}");
+            }
+
+            try
+            {
+                elevation = (double)oldObject["elevation"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the elevation of the Location. returning elevation as default ({elevation}).\n The error: {ex}");
+            }
+
+            try
+            {
+                stationID = (string)oldObject["station_id"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the station ID of the Location. returning station ID as default (\"\").\n The error: {ex}");
+            }
+
+            try
+            {
+                source = (string)oldObject["source"];
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError($"An error occurred when reading the source of the Location. returning source as default ({source}).\n The error: {ex}");
+            }
+
+            return new oM.LadybugTools.Location()
+            {
+                City = city,
+                State = state,
+                Country = country,
+                Latitude = latitude,
+                Longitude = longitude,
+                TimeZone = timeZone,
+                Elevation = elevation,
+                StationId = stationID,
+                Source = source
+            };
         }
 
         public static Dictionary<string, object> FromLocation(BH.oM.LadybugTools.Location location)
