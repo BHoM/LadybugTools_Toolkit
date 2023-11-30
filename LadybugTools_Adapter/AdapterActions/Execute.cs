2637458d0a4f2450120efd45703938e93914b7f8
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-30 10:50:27 +0000

    added logic to RunCommand commands

--- LadybugTools_Adapter/AdapterActions/Execute.cs
+++ LadybugTools_Adapter/AdapterActions/Execute.cs
@@ -1,9 +1,15 @@
-﻿using BH.oM.Adapter;
+﻿using BH.Engine.Adapter;
+using BH.Engine.LadybugTools;
+using BH.oM.Adapter;
 using BH.oM.Adapter.Commands;
 using BH.oM.Base;
+using BH.oM.Data.Requests;
 using BH.oM.LadybugTools;
+using BH.oM.Python;
+using BH.Engine.Python;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -15,39 +21,7 @@ namespace BH.Adapter.LadybugTools
         {
             Output<List<object>, bool> output = new Output<List<object>, bool>() { Item1 = new List<object>(), Item2 = false };
 
-            if (command == null)
-            {
-                BH.Engine.Base.Compute.RecordError("Please input a valid ILadybugCommand to execute.");
-                return output;
-            }
-
-            ILadybugCommand ladybugCommand = command as ILadybugCommand;
-            if (ladybugCommand == null)
-            {
-                BH.Engine.Base.Compute.RecordError($"The command {command.GetType().FullName} is not valid for the LadybugTools Adapter. Please use a LadybugCommand, or use the correct adapter for the input command.");
-                return output;
-            }
-
-            if (actionConfig == null)
-            {
-                BH.Engine.Base.Compute.RecordError("Please provide a valid LadybugConfig to use with this adapter.");
-                return output;
-            }
-            
-            LadybugConfig config = actionConfig as LadybugConfig;
-            if (config == null)
-            {
-                BH.Engine.Base.Compute.RecordError($"The ActionConfig provided: {actionConfig.GetType().FullName} is not valid for the LadybugTools Adapter. Please use a valid LadybugConfig.");
-                return output;
-            }
-
-            if (config.JsonFile == null)
-            {
-                BH.Engine.Base.Compute.RecordError($"Please provide a valid JsonFile FileSettings object in the LadybugConfig.");
-                return output;
-            }
-
-            List<object> temp = RunCommand(command as dynamic, config);
+            List<object> temp = IRunCommand(command);
             bool success = !(BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == oM.Base.Debugging.EventType.Error).Any());
             output.Item1 = temp;
             output.Item2 = success;
@@ -61,51 +35,199 @@ namespace BH.Adapter.LadybugTools
         /* Public methods - Interface                     */
         /**************************************************/
 
-        public List<object> IRunCommand(IExecuteCommand command, LadybugConfig config)
+        public List<object> IRunCommand(IExecuteCommand command)
         {
             if (command == null)
             {
-                BH.Engine.Base.Compute.RecordError("Please input a valid ILadybugCommand to execute.");
+                BH.Engine.Base.Compute.RecordError("Please input a valid Ladybug Command to execute.");
                 return new List<object>();
             }
-            return RunCommand(command as dynamic, config);
+            return RunCommand(command as dynamic);
         }
 
         /**************************************************/
         /* Private methods - Ladybug Commands             */
         /**************************************************/
 
-        private List<object> RunCommand(GetMaterialCommand command, LadybugConfig config)
+        private List<object> RunCommand(GetMaterialCommand command)
         {
-            return new List<object>();
+            PythonEnvironment env = Engine.LadybugTools.Compute.InstallPythonEnv_LBT(true);
+            LadybugConfig config = new LadybugConfig()
+            {
+                JsonFile = new FileSettings()
+                {
+                    FileName = $"LBTBHoM_Materials_{DateTime.Now:yyyyMMdd}.json",
+                    Directory = Path.GetTempPath()
+                }
+            };
+
+            if (!File.Exists(config.JsonFile.GetFullFileName()))
+            {
+                string script = Path.Combine(Engine.Python.Query.DirectoryCode(), "LadybugTools_Toolkit\\src\\ladybugtools_toolkit\\bhom\\wrapped", "get_material.py");
+
+                string cmdCommand = $"{env.Executable} {script} -j \"{config.JsonFile.GetFullFileName()}\"";
+
+                Engine.Python.Compute.RunCommandStdout(command: cmdCommand, hideWindows: true);
+            }
+
+            List<object> materialObjects = Pull(new FilterRequest(), actionConfig: config).ToList();
+
+            return materialObjects.Where(m => (m as IEnergyMaterialOpaque).Name.Contains(command.Filter)).ToList();
         }
 
         /**************************************************/
 
-        private List<object> RunCommand(GetTypologyCommand command, LadybugConfig config)
+        private List<object> RunCommand(GetTypologyCommand command)
         {
-            return new List<object>();
+            PythonEnvironment env = Engine.LadybugTools.Compute.InstallPythonEnv_LBT(true);
+            LadybugConfig config = new LadybugConfig()
+            {
+                JsonFile = new FileSettings()
+                {
+                    FileName = $"LBTBHoM_Typologies_{DateTime.Now:yyyyMMdd}.json",
+                    Directory = Path.GetTempPath()
+                }
+            };
+
+            if (!File.Exists(config.JsonFile.GetFullFileName()))
+            {
+                string script = Path.Combine(Engine.Python.Query.DirectoryCode(), "LadybugTools_Toolkit\\src\\ladybugtools_toolkit\\bhom\\wrapped", "get_typology.py");
+
+                string cmdCommand = $"{env.Executable} {script} -j \"{config.JsonFile.GetFullFileName()}\"";
+
+                Engine.Python.Compute.RunCommandStdout(command: cmdCommand, hideWindows: true);
+            }
+
+            List<object> typologyObjects = Pull(new FilterRequest(), actionConfig: config).ToList();
+
+            return typologyObjects.Where(m => (m as Typology).Name.Contains(command.Filter)).ToList();
         }
 
         /**************************************************/
 
-        private List<object> RunCommand(RunSimulationCommand command, LadybugConfig config)
+        private List<object> RunCommand(RunSimulationCommand command)
         {
-            return new List<object>();
+            // validation prior to passing to Python
+            if (command.EpwFile == null)
+            {
+                BH.Engine.Base.Compute.RecordError($"{nameof(command.EpwFile)} input cannot be null.");
+                return null;
+            }
+
+            if (!File.Exists(command.EpwFile.GetFullFileName()))
+            {
+                BH.Engine.Base.Compute.RecordError($"{command.EpwFile.GetFullFileName()} does not exist.");
+                return null;
+            }
+
+            if (command.GroundMaterial == null)
+            {
+                BH.Engine.Base.Compute.RecordError($"{nameof(command.GroundMaterial)} input cannot be null.");
+                return null;
+            }
+
+            if (command.ShadeMaterial == null)
+            {
+                BH.Engine.Base.Compute.RecordError($"{nameof(command.ShadeMaterial)} input cannot be null.");
+                return null;
+            }
+
+            // construct adapter and config
+            LadybugConfig config = new LadybugConfig()
+            {
+                JsonFile = new FileSettings()
+                {
+                    FileName = $"LBTBHoM_{Guid.NewGuid()}.json",
+                    Directory = Path.GetTempPath()
+                }
+            };
+
+            // construct the base object and file to be passed to Python for simulation
+            SimulationResult simulationResult = new SimulationResult()
+            {
+                EpwFile = Path.GetFullPath(command.EpwFile.GetFullFileName()).Replace(@"\", "/"),
+                GroundMaterial = command.GroundMaterial,
+                ShadeMaterial = command.ShadeMaterial,
+                Name = Engine.LadybugTools.Compute.SimulationID(command.EpwFile.GetFullFileName(), command.GroundMaterial, command.ShadeMaterial)
+            };
+
+            // push object to json file
+            Push(new List<SimulationResult>() { simulationResult }, actionConfig: config);
+
+            // locate the Python executable and file containing the simulation code
+            PythonEnvironment env = Engine.LadybugTools.Compute.InstallPythonEnv_LBT(true);
+            string script = Path.Combine(Engine.Python.Query.DirectoryCode(), "LadybugTools_Toolkit\\src\\ladybugtools_toolkit\\bhom\\wrapped", "simulation_result.py");
+
+            // run the simulation
+            string cmdCommand = $"{env.Executable} {script} -j \"{config.JsonFile.GetFullFileName()}\"";
+            Engine.Python.Compute.RunCommandStdout(command: cmdCommand, hideWindows: true);
+
+            // reload from Python results
+            List<object> simulationResultPopulated = Pull(new FilterRequest(), actionConfig: config).ToList();
+
+            // remove temporary file
+            File.Delete(config.JsonFile.GetFullFileName());
+
+            return simulationResultPopulated;
         }
 
         /**************************************************/
 
-        private List<object> RunCommand(RunExternalComfortCommand command, LadybugConfig config)
+        private List<object> RunCommand(RunExternalComfortCommand command)
         {
-            return new List<object>();
+            if (command.SimulationResult == null)
+            {
+                BH.Engine.Base.Compute.RecordError($"{nameof(command.SimulationResult)} input cannot be null.");
+                return null;
+            }
+
+            if (command.Typology == null)
+            {
+                BH.Engine.Base.Compute.RecordError($"{nameof(command.Typology)} input cannot be null.");
+                return null;
+            }
+
+            LadybugConfig config = new LadybugConfig()
+            {
+                JsonFile = new FileSettings()
+                {
+                    FileName = $"LBTBHoM_{Guid.NewGuid()}.json",
+                    Directory = Path.GetTempPath()
+                }
+            };
+
+            // construct the base object
+            ExternalComfort externalComfort = new ExternalComfort()
+            {
+                SimulationResult = command.SimulationResult,
+                Typology = command.Typology,
+            };
+
+            // push objects to json file
+            Push(new List<ExternalComfort>() { externalComfort }, actionConfig: config);
+
+            // locate the Python executable and file containing the simulation code
+            PythonEnvironment env = Engine.LadybugTools.Compute.InstallPythonEnv_LBT(true);
+            string script = Path.Combine(Engine.Python.Query.DirectoryCode(), "LadybugTools_Toolkit\\src\\ladybugtools_toolkit\\bhom\\wrapped", "external_comfort.py");
+
+            // run the calculation
+            string cmdCommand = $"{env.Executable} {script} -j \"{config.JsonFile.GetFullFileName()}\"";
+            Engine.Python.Compute.RunCommandStdout(command: cmdCommand, hideWindows: true);
+
+            // reload from Python results
+            List<object> externalComfortPopulated = Pull(new FilterRequest(), actionConfig: config).ToList();
+
+            // remove temporary file
+            File.Delete(config.JsonFile.GetFullFileName());
+
+            return externalComfortPopulated;
         }
 
         /**************************************************/
         /* Private methods - Fallback                     */
         /**************************************************/
 
-        private List<object> RunCommand(IExecuteCommand command, LadybugConfig config)
+        private List<object> RunCommand(IExecuteCommand command)
         {
             BH.Engine.Base.Compute.RecordError($"The command {command.GetType().FullName} is not valid for the LadybugTools Adapter. Please use a LadybugCommand, or use the correct adapter for the input command.");
             return new List<object>();
