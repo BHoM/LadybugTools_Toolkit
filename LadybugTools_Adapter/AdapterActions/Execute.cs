c6f228f02af41eb6100bd9b06f8862ccc3501b5e
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2023-11-30 09:59:03 +0000

    added RunCommand interface methods

--- LadybugTools_Adapter/AdapterActions/Execute.cs
+++ LadybugTools_Adapter/AdapterActions/Execute.cs
@@ -4,6 +4,7 @@ using BH.oM.Base;
 using BH.oM.LadybugTools;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BH.Adapter.LadybugTools
@@ -46,8 +47,68 @@ namespace BH.Adapter.LadybugTools
                 return output;
             }
 
+            List<object> temp = RunCommand(command as dynamic, config);
+            bool success = !(BH.Engine.Base.Query.CurrentEvents().Where(x => x.Type == oM.Base.Debugging.EventType.Error).Any());
+            output.Item1 = temp;
+            output.Item2 = success;
 
-            return new Output<List<object>, bool>();
+            return output;
+        }
+
+
+
+        /**************************************************/
+        /* Public methods - Interface                     */
+        /**************************************************/
+
+        public List<object> IRunCommand(IExecuteCommand command, LadybugConfig config)
+        {
+            if (command == null)
+            {
+                BH.Engine.Base.Compute.RecordError("Please input a valid ILadybugCommand to execute.");
+                return new List<object>();
+            }
+            return RunCommand(command as dynamic, config);
+        }
+
+        /**************************************************/
+        /* Private methods - Ladybug Commands             */
+        /**************************************************/
+
+        private List<object> RunCommand(GetMaterialCommand command, LadybugConfig config)
+        {
+            return new List<object>();
+        }
+
+        /**************************************************/
+
+        private List<object> RunCommand(GetTypologyCommand command, LadybugConfig config)
+        {
+            return new List<object>();
+        }
+
+        /**************************************************/
+
+        private List<object> RunCommand(RunSimulationCommand command, LadybugConfig config)
+        {
+            return new List<object>();
+        }
+
+        /**************************************************/
+
+        private List<object> RunCommand(RunExternalComfortCommand command, LadybugConfig config)
+        {
+            return new List<object>();
+        }
+
+        /**************************************************/
+        /* Private methods - Fallback                     */
+        /**************************************************/
+
+        private List<object> RunCommand(IExecuteCommand command, LadybugConfig config)
+        {
+            BH.Engine.Base.Compute.RecordError($"The command {command.GetType().FullName} is not valid for the LadybugTools Adapter. Please use a LadybugCommand, or use the correct adapter for the input command.");
+            return new List<object>();
         }
     }
 }
