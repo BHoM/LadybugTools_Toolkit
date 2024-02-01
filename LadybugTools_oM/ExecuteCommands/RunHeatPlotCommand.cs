22e53e270a25d306c9e9e1a6c8a7ace66a72507e
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-02-01 09:33:09 +0000

    added description attributes to the HeatPlot command

--- LadybugTools_oM/ExecuteCommands/RunHeatPlotCommand.cs
+++ LadybugTools_oM/ExecuteCommands/RunHeatPlotCommand.cs
@@ -1,15 +1,24 @@
 ﻿using BH.oM.Adapter;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BH.oM.LadybugTools
 {
+    [Description("Command that, when executed with the LadybugToolsAdapter, produces a heatmap from data in an epw file.\nOutput is a string of either the path to the image (if OutputLocation is not set) or the base 64 string representation of it.")]
     public class RunHeatPlotCommand : IExecuteCommand
     {
+        [Description("The path to an EPW file.")]
         public virtual string EpwFile { get; set; } = "";
+
+        [Description("Key representing an hourly continuous collection in the EPW file to plot.")]
         public virtual EpwKey EpwKey { get; set; } = EpwKey.Undefined;
+
+        [Description("A Matplotlib colout map. Corresponds to the 'cmap' parameter of plot methods. See https://matplotlib.org/stable/users/explain/colors/colormaps.html for examples of valid keys. Default of 'viridis'.")]
         public virtual string ColourMap { get; set; } = "viridis";
+
+        [Description("Full file path (with file name) to save the plot to. Leave blank to output a base 64 string representation of the image instead.")]
         public virtual string OutputLocation { get; set; } = "";
     }
 }
