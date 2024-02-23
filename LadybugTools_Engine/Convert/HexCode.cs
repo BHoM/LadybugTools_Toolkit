f3b9dc9b405629f8b5f228b3f0dbc48e3bb04c34
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-02-23 10:06:29 +0000

    added descriptions to colour convert methods

--- LadybugTools_Engine/Convert/HexCode.cs
+++ LadybugTools_Engine/Convert/HexCode.cs
@@ -27,49 +27,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Globalization;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
 
 namespace BH.Engine.LadyBugTools
 {
     public static partial class Convert
     {
+        [Description("Converts a colour to its respective RGB hexadecimal code (eg. white => #ffffff).")]
+        [Input("colour", "The colour to convert into a hex code.")]
+        [Output("hex", "The corresponding hex code.")]
         public static string ToHexCode(this Color colour)
         {
             return $"#{colour.R.ToString("X2")}{colour.G.ToString("X2")}{colour.B.ToString("X2")}";
         }
 
-        public static Color? FromHexCode(this string colour)
+        [Description("Converts a string that is in the RGB hexadecimal format into a colour. (eg. #ffffff => white).")]
+        [Input("hex", "The hexadecimal representation of a colour.")]
+        [Output("colour", "The corresponding colour.")]
+        public static Color? FromHexCode(this string hex)
         {
-            if (colour.IsNullOrEmpty())
+            if (hex.IsNullOrEmpty())
             {
                 BH.Engine.Base.Compute.RecordError("Cannot create a colour from an empty string.");
                 return null;
             }
 
-            colour = colour.ToLower();
+            hex = hex.ToLower();
 
-            if (colour[0] == '#')
+            if (hex[0] == '#')
             {
-                colour = colour.Substring(1);
+                hex = hex.Substring(1);
             }
 
-            if (colour.Length != 6)
+            if (hex.Length != 6)
             {
-                BH.Engine.Base.Compute.RecordError($"The input string: {colour}, is invalid to create a colour from. Hex codes must be 6 characters long, and only contain numbers (0-9) and the characters a to f.");
+                BH.Engine.Base.Compute.RecordError($"The input string: {hex}, is invalid to create a colour from. Hex codes must be 6 characters long, and only contain numbers (0-9) and the characters a to f.");
                 return null;
             }
 
             List<char> chars = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
-            if (!colour.All(x => chars.Contains(x)))
+            if (!hex.All(x => chars.Contains(x)))
             {
-                BH.Engine.Base.Compute.RecordError($"The input string: {colour}, is invalid to create a colour from. Hex codes must be 6 characters long, and only contain numbers (0-9) and the characters a to f.");
+                BH.Engine.Base.Compute.RecordError($"The input string: {hex}, is invalid to create a colour from. Hex codes must be 6 characters long, and only contain numbers (0-9) and the characters a to f.");
                 return null;
             }
 
             return Color.FromArgb(
-                red: int.Parse(colour.Substring(0, 2), NumberStyles.HexNumber),
-                green: int.Parse(colour.Substring(2, 2), NumberStyles.HexNumber),
-                blue: int.Parse(colour.Substring(4, 2), NumberStyles.HexNumber));
+                red: int.Parse(hex.Substring(0, 2), NumberStyles.HexNumber),
+                green: int.Parse(hex.Substring(2, 2), NumberStyles.HexNumber),
+                blue: int.Parse(hex.Substring(4, 2), NumberStyles.HexNumber));
         }
     }
 }
