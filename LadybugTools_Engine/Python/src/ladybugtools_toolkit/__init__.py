62c4518d21d050ba8fb51f9f2ea989ecb58431e0
Tristan Gerrish <Tristan.Gerrish@burohappold.com>
2024-11-07 10:52:40 +0000

    Added SRI estimation methods

--- LadybugTools_Engine/Python/src/ladybugtools_toolkit/__init__.py
+++ LadybugTools_Engine/Python/src/ladybugtools_toolkit/__init__.py
@@ -4,12 +4,23 @@ import getpass
 import os
 from pathlib import Path
 
-# pylint: disable=E0401
-
 import matplotlib.pyplot as plt
 
+# pylint: disable=E0401
+
+# get common paths
+DATA_DIRECTORY = (Path(__file__).parent.parent / "data").absolute()
+BHOM_DIRECTORY = (Path(__file__).parent / "bhom").absolute()
+HOME_DIRECTORY = (Path("C:/Users/") / getpass.getuser()).absolute()
+
 # override "HOME" in case IT has set this to something other than default
-os.environ["HOME"] = (Path("C:/Users/") / getpass.getuser()).as_posix()
+os.environ["HOME"] = HOME_DIRECTORY.as_posix()
 
 # set plotting style for modules within this toolkit
-plt.style.use(Path(__file__).parent / "bhom" / "bhom.mplstyle")
+plt.style.use(BHOM_DIRECTORY / "bhom.mplstyle")
+
+# get dataset paths
+SRI_DATA = DATA_DIRECTORY / "sri_data.csv"
+KOEPPEN_DATA = DATA_DIRECTORY / "koeppen.csv"
+ICE_MATERIALS_DATA = DATA_DIRECTORY / "ICE_database_sources.xlsx.csv"
+VEGETATION_DATA = DATA_DIRECTORY / "vegetation.json"
