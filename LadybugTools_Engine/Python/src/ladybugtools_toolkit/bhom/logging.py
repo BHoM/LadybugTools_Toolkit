ed52bbec58274149266cb57623c56d6cb912921a
Thomas Edward Kingstone <Tom.Kingstone@burohappold.com>
2024-12-05 13:36:26 +0000

    forgot to save single line

--- LadybugTools_Engine/Python/src/ladybugtools_toolkit/bhom/logging.py
+++ LadybugTools_Engine/Python/src/ladybugtools_toolkit/bhom/logging.py
@@ -1,7 +1,7 @@
 ﻿from python_toolkit.bhom.logging import CONSOLE_LOGGER as PYTHON_TOOLKIT_LOGGER
 import logging
 
-TOOLKIT_NAME = "LadybugTools_Toolkit"
+from .. import TOOLKIT_NAME
 
 CONSOLE_LOGGER = logging.getLogger(f"{TOOLKIT_NAME}[console]")
 CONSOLE_LOGGER.parent = PYTHON_TOOLKIT_LOGGER
